792f55e00a7c04d52f3c010050a386b49c650b29
Yabuki <syodai829@icloud.com>
2021-10-27 14:43:10 +0900

    Initial Commit

---
+++ api-django/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  api-django
+//
+//  Created by Yabuki Shodai on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
